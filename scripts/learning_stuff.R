803dfabfe91a020e7da9e14b59e89c587bd4b34b
Conor Tompkins <conorotompkins@gmail.com>
2017-12-25 16:11:34 -0500

    created a section for learning

---
+++ scripts/learning_stuff.R
@@ -0,0 +1,18 @@
+#Objective: Learn how to do stuff
+
+#= tells it something is equal
+#== asks if it is equal
+
+2 == 2
+
+#TRUE = 1, FALSE = 0
+
+TRUE+TRUE
+TRUE+FALSE
+TRUE-TRUE
+
+1+NA
+
+sum(c(1,NA,5))
+sum(1,NA,5)
+sum(1,NA,5,na.rm=TRUE)
